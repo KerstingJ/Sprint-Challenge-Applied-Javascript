917cba9dd95c39a2e7a02082d73b729c7f8c3032
KerstingJ <Kersting.Josh@Gmail.com>
2019-03-08 12:25:03 -0500

    added carousel functionality

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,9 +1,3 @@
-class Carousel {
-
-}
-
-let carousel = document.querySelector();
-
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
     2. You will need to grab a reference to all of the images
@@ -12,3 +6,52 @@ let carousel = document.querySelector();
     5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
+
+class Carousel {
+    constructor(element){
+        this.element = element;
+        this.btnLeft = this.element.querySelector(".left-button");
+        this.btnRight = this.element.querySelector(".right-button");
+        this.images = this.element.querySelectorAll("img");
+        this.currentImgId = 0;
+        this.images[0].style.display = "block";
+        this.btnLeft.addEventListener("mousedown", this.contLeft.bind(this));
+        this.btnRight.addEventListener("mousedown", this.contRight.bind(this));
+    }
+    contLeft(){
+        let event = setInterval(this.left.bind(this), 800)
+        this.left.bind(this)
+        let myFunc = ()=>{
+            clearInterval(event)
+            this.btnLeft.removeEventListener("mouseup", myFunc);
+        }
+
+        this.btnLeft.addEventListener("mouseup", myFunc);
+    }
+    contRight(){
+        let event = setInterval(this.right.bind(this), 800)
+        this.right.bind(this)
+        let myFunc = ()=>{
+            clearInterval(event)
+            this.btnRight.removeEventListener("mouseup", myFunc);
+        }
+
+        this.btnRight.addEventListener("mouseup", myFunc);
+    }
+
+    left(){
+        this.images[this.currentImgId].style.display =  null;
+        let newId = this.currentImgId - 1;
+        this.currentImgId = newId < 0 ? this.images.length - 1 : newId;
+        this.images[this.currentImgId].style.display =  "block";
+    }
+    right(){
+        this.images[this.currentImgId].style.display =  null;
+        let newId = this.currentImgId + 1;
+        this.currentImgId = newId > this.images.length - 1 ? 0 : newId;
+        this.images[this.currentImgId].style.display =  "block";
+    }
+
+}
+
+let carousel = new Carousel(document.querySelector(".carousel"));
