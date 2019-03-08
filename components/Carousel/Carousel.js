f039c6e96f1693edecb78bfb0ceb6871ffafaa79
KerstingJ <Kersting.Josh@Gmail.com>
2019-03-08 16:14:37 -0500

    Added comments

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -10,50 +10,56 @@
 class Carousel {
     constructor(element){
         this.element = element;
+
         this.btnLeft = this.element.querySelector(".left-button");
         this.btnRight = this.element.querySelector(".right-button");
+
         this.images = this.element.querySelectorAll("img");
         this.currentImgId = 0;
         this.images[0].style.display = "block";
+
         this.btnLeft.addEventListener("mousedown", this.contLeft.bind(this));
         this.btnRight.addEventListener("mousedown", this.contRight.bind(this));
     }
+
     contLeft(){
         this.left();// rotate on click
-        let event = setInterval(this.left.bind(this), 800);
+        let event = setInterval(this.left.bind(this), 800); //rotate every .8s
 
-        let stop = ()=>{
+        let stop = ()=>{ //removes our listener and the interval
             clearInterval(event)
-            this.btnLeft.removeEventListener("mouseup", stop);
+            //we're on the 
+            window.removeEventListener("mouseup", stop);
         }
 
-        this.btnLeft.addEventListener("mouseup", stop);
+        window.addEventListener("mouseup", stop); // on window so rotation stops if your mouse moves off button
     }
+    
     contRight(){
         this.right();
         let event = setInterval(this.right.bind(this), 800);
 
         let stop = ()=>{
             clearInterval(event)
-            this.btnRight.removeEventListener("mouseup", stop);
+            window.removeEventListener("mouseup", stop);
         }
 
-        this.btnRight.addEventListener("mouseup", stop);
+        window.addEventListener("mouseup", stop);
     }
 
     left(){
-        this.images[this.currentImgId].style.display =  null;
-        let newId = this.currentImgId - 1;
-        this.currentImgId = newId < 0 ? this.images.length - 1 : newId;
-        this.images[this.currentImgId].style.display =  "block";
+        this.images[this.currentImgId].style.display =  null; // get our original img set it to default style
+        let newId = this.currentImgId - 1; // get the id of our new img
+        this.currentImgId = newId < 0 ? this.images.length - 1 : newId; // make sure id falls in our array.
+        this.images[this.currentImgId].style.display =  "block"; // set our new active image
     }
+
     right(){
         this.images[this.currentImgId].style.display =  null;
         let newId = this.currentImgId + 1;
         this.currentImgId = newId > this.images.length - 1 ? 0 : newId;
         this.images[this.currentImgId].style.display =  "block";
     }
-
 }
 
 let carousel = new Carousel(document.querySelector(".carousel"));
